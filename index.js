91edae0ca19d33a99824ddc3f5a30006562577c9
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-01-01 10:59:53 +0300

    Root Stack architecture is created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 import Router from './src/Router';
-import SignInScreen from "./src/screens/SignInScreen";
+import SignInScreen from "./src/screens/RootStack/SignInScreen";
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import SignInScreen from './screens/SignInScreen';
+import SignInScreen from './screens/RootStack/SignInScreen';
 import {createStackNavigator} from '@react-navigation/stack';
-import RootStackScreen from './screens/RootStackScreen';
+import RootStackScreen from './screens/RootStack/RootStackScreen';
 const Stack = createStackNavigator();
 
 function Router() {
