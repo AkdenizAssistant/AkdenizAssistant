9d4b7011c93e15a7d98df10bce0eb30720d5bf1c
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-01-08 12:38:56 +0300

    Header Bar is added as a component.

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+export default function HeaderBar() {
+  return (
+    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+      <TouchableOpacity style={styles.menu}>
+        <Image source={require('../assets/MenuIcon.png')} />
+      </TouchableOpacity>
+      <Text style={styles.title}>  Akdeniz Asistan</Text>
+      <TouchableOpacity style={styles.notification}>
+        <Image source={require('../assets/notification.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.profile}>
+        <Image source={require('../assets/user1.png')} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Quicksand',
+    fontSize: 32,
+    fontStyle: 'normal',
+    fontWeight: '900',
+    lineHeight: 53,
+    letterSpacing: 0,
+    textAlign: 'center',
+    flex: 256,
+  },
+  notification: {
+    flex: 42,
+    alignItems: 'center',
+  },
+  menu: {
+    flex: 45,
+    alignItems: 'center',
+  },
+  profile: {
+    flex: 42,
+    alignItems: 'center',
+  },
+});
--- src/screens/HomeStackScreen.js
+++ src/screens/HomeStackScreen.js
@@ -1,19 +1,10 @@
 import * as React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import HeaderBar from '../components/HeaderBar';
 
 const HomeStack = createStackNavigator();
 
-function LogoTitle() {
-  return (
-    <View style={{flex: 1, flexDirection: 'row'}}>
-      <Text style={styles.menu}>menu</Text>
-      <Text style={styles.title}>Akdeniz Asistan</Text>
-      <Text style={styles.notification}>notification</Text>
-      <Text style={styles.profile}>profile</Text>
-    </View>
-  );
-}
 const SettingsScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -32,7 +23,7 @@ const HomeStackScreen = ({navigation}) => (
       headerTitleStyle: {
         fontWeight: 'bold',
       },
-      headerTitle: (props) => <LogoTitle {...props} />,
+      headerTitle: (props) => <HeaderBar {...props} />,
     }}>
     <HomeStack.Screen name="Home" component={SettingsScreen} />
     <HomeStack.Screen
@@ -46,24 +37,3 @@ const HomeStackScreen = ({navigation}) => (
 );
 
 export default HomeStackScreen;
-const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'Quicksand',
-    fontSize: 32,
-    fontStyle: 'normal',
-    fontWeight: '900',
-    lineHeight: 53,
-    letterSpacing: 0,
-    textAlign: 'center',
-    flex: 256,
-  },
-  notification: {
-    flex: 42,
-  },
-  menu: {
-    flex: 56,
-  },
-  profile: {
-    flex: 42,
-  },
-});
