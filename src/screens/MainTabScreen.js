92d08fda3116cacaa040715c41fd57706fc3e8b4
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-01-09 08:46:53 +0300

    Tab Bar is updated

--- src/screens/MainTabScreen.js
+++ src/screens/MainTabScreen.js
@@ -1,33 +1,54 @@
 import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {StyleSheet, Text, View, Image, useWindowDimensions} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
+import {useSafeArea} from 'react-native-safe-area-context';
 import HomeStackScreen from './HomeStackScreen';
 
 const HomeStack = createStackNavigator();
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-function LogoTitle() {
-  return <Text style={styles.title}>Akdeniz Asistan</Text>;
-}
 export default function MainTabScreen() {
+  const windowWidth = useWindowDimensions().width;
+  const insets = useSafeArea();
   return (
     <Tab.Navigator
       initialRouteName="Home"
       barStyle={{
-        height: 80,
         textAlign: 'center',
         alignItems: 'center',
-        backgroundColor: '#F47325',
+        backgroundColor: '',
+        width: windowWidth,
+      }}
+      tabBarOptions={{
+        style: {
+          height: 60 + insets.bottom,
+        },
+        tabStyle: {
+          height: 60,
+        },
+        height: 60 + insets.bottom,
+        showLabel: false,
+        activeBackgroundColor:'rgba(244, 115, 37, 0.81)',
+        inactiveBackgroundColor: '#F47325',
       }}>
       <Tab.Screen
         name="Home"
         barStyle={{
-          backgroundColor: '#F47325',
           textAlign: 'center',
           alignItems: 'center',
         }}
         component={HomeStackScreen}
+        showLabel={false}
+        options={{
+          tabBarLabel: 'refectory',
+          tabBarIcon: () => (
+            <Image
+              style={{height: 40, width:40}}
+              source={require('../assets/homePageIcon.png')}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="refectory"
@@ -35,31 +56,42 @@ export default function MainTabScreen() {
         component={HomeStackScreen}
         options={{
           tabBarLabel: 'refectory',
+          tabBarIcon: () => (
+            <Image
+              style={{height: 40, width:40}}
+              source={require('../assets/refectoryIcon.png')}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="lectures"
-        barStyle={{height: 80, backgroundColor: '#F47325', textAlign: 'center'}}
+        barStyle={{textAlign: 'center'}}
         component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'refectory',
+          tabBarIcon: () => (
+            <Image
+              style={{height: 40, width:40}}
+              source={require('../assets/lecturersIcon.png')}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="advertisement"
-        barStyle={{height: 80, backgroundColor: '#F47325', textAlign: 'center'}}
+        barStyle={{textAlign: 'center'}}
         component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'refectory',
+          tabBarIcon: () => (
+            <Image
+              style={{height: 40, width:40}}
+              source={require('../assets/advertisingIcon.png')}
+            />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'Quicksand',
-    fontSize: 32,
-    fontStyle: 'normal',
-    fontWeight: '900',
-    lineHeight: 53,
-    letterSpacing: 0,
-    textAlign: 'center',
-    flex: 1,
-  },
-});
